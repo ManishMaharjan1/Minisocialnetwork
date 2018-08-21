27e90df78add2972ee05b4c65515d9d7b2d859a1
manish maharjan <maharjanmia@gmail.com>
2018-08-21 11:37:25 +0545

    first commit

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>4040.</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="test/css">
+
+    <style>
+        html,body{
+            height: 100%;
+        }
+
+        body {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            color: #B0BEC5;
+            display: table;
+            font-weight: 100;
+            font-family: 'Lato', sans-serif;
+        }
+
+        .container {
+            text-align: center;
+            display:  table-cell;   
+            vertical-align: middle;
+        }
+
+        .content {
+            text-align: center;
+            display: inline-block;
+        }
+
+        .title {
+            font-size: 72px;
+            margin-bottom: 40px;
+        }
+    </style>
+</head>
+<body>
+   <div class="container">
+        <div class="content">
+            <div class="title">Nothing was found! Sorry :(</div>
+        </div>
+   </div>
+</body>
+</html>
